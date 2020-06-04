44ecd48c9525c3a93e17ee84a4299f3c9d7b78eb
riccardo4zan <forzanric@gmail.com>
2020-06-04 13:27:28 +0200

    Rimozione commenti classe privata interna

--- src/adapters/ListAdapter.java
+++ src/adapters/ListAdapter.java
@@ -449,30 +449,11 @@ public class ListAdapter implements HList {
             this.limit = limit;
         }
 
-        /**
-         * Returns {@code true} if this list iterator has more elements when
-         * traversing the list in the forward direction. (In other words,
-         * returns {@code true} if next() would return an element rather
-         * than throwing an exception.)
-         *
-         * @return {@code true} if the list iterator has more elements when
-         * traversing the list in the forward direction
-         */
         @Override
         public boolean hasNext() {
             return cursor<limit;
         }
 
-        /**
-         * Returns the next element in the list and advances the cursor position.
-         * This method may be called repeatedly to iterate through the list,
-         * or intermixed with calls to previous() to go back and forth.
-         * (Note that alternating calls to {@code next} and {@code previous}
-         * will return the same element repeatedly.)
-         *
-         * @return the next element in the list
-         * @throws NoSuchElementException if the iteration has no next element
-         */
         @Override
         public Object next() {
             if(!this.hasNext()) throw new NoSuchElementException();
@@ -480,32 +461,11 @@ public class ListAdapter implements HList {
             return ve.get(actual);
         }
 
-        /**
-         * Returns {@code true} if this list iterator has more elements when
-         * traversing the list in the reverse direction.  (In other words,
-         * returns {@code true} if previous() would return an element
-         * rather than throwing an exception.)
-         *
-         * @return {@code true} if the list iterator has more elements when
-         * traversing the list in the reverse direction
-         */
         @Override
         public boolean hasPrevious() {
             return cursor>0;
         }
 
-        /**
-         * Returns the previous element in the list and moves the cursor
-         * position backwards.  This method may be called repeatedly to
-         * iterate through the list backwards, or intermixed with calls to
-         * next() to go back and forth.  (Note that alternating calls
-         * to {@code next} and {@code previous} will return the same
-         * element repeatedly.)
-         *
-         * @return the previous element in the list
-         * @throws NoSuchElementException if the iteration has no previous
-         *                                element
-         */
         @Override
         public Object previous() {
             if(!this.hasPrevious()) throw new NoSuchElementException();
@@ -513,46 +473,16 @@ public class ListAdapter implements HList {
             return ve.get(actual);
         }
 
-        /**
-         * Returns the index of the element that would be returned by a
-         * subsequent call to next(). (Returns list size if the list
-         * iterator is at the end of the list.)
-         *
-         * @return the index of the element that would be returned by a
-         * subsequent call to {@code next}, or list size if the list
-         * iterator is at the end of the list
-         */
         @Override
         public int nextIndex() {
             return cursor;
         }
 
-        /**
-         * Returns the index of the element that would be returned by a
-         * subsequent call to previous(). (Returns -1 if the list
-         * iterator is at the beginning of the list.)
-         *
-         * @return the index of the element that would be returned by a
-         * subsequent call to {@code previous}, or -1 if the list
-         * iterator is at the beginning of the list
-         */
         @Override
         public int previousIndex() {
             return cursor-1;
         }
 
-        /**
-         * Removes from the list the last element that was returned by
-         * next() or previous() (optional operation).  This call can
-         * only be made once per call to {@code next} or {@code previous}.
-         * It can be made only if add() has not been
-         * called after the last call to {@code next} or {@code previous}.
-         *
-         * @throws IllegalStateException         if neither {@code next} nor
-         *                                       {@code previous} have been called, or {@code remove} or
-         *                                       {@code add} have been called after the last call to
-         *                                       {@code next} or {@code previous}
-         */
         @Override
         public void remove() {
             if(actual==-1) throw new exception.IllegalStateException();
@@ -562,23 +492,6 @@ public class ListAdapter implements HList {
             actual = -1;
         }
 
-        /**
-         * Replaces the last element returned by next() or
-         * previous() with the specified element (optional operation).
-         * This call can be made only if neither remove() nor
-         * add() have been called after the last call to {@code next} or
-         * {@code previous}.
-         *
-         * @param o the element with which to replace the last element returned by
-         *          {@code next} or {@code previous}
-         *
-         * @throws IllegalArgumentException      if some aspect of the specified
-         *                                       element prevents it from being added to this list
-         * @throws IllegalStateException         if neither {@code next} nor
-         *                                       {@code previous} have been called, or {@code remove} or
-         *                                       {@code add} have been called after the last call to
-         *                                       {@code next} or {@code previous}
-         */
         @Override
         public void set(Object o) {
             if(actual==-1) throw new IllegalStateException();
@@ -587,22 +500,6 @@ public class ListAdapter implements HList {
 
         }
 
-        /**
-         * Inserts the specified element into the list (optional operation).
-         * The element is inserted immediately before the next element that
-         * would be returned by next(), if any, and after the next
-         * element that would be returned by previous(), if any.  (If the
-         * list contains no elements, the new element becomes the sole element
-         * on the list.)  The new element is inserted before the implicit
-         * cursor: a subsequent call to {@code next} would be unaffected, and a
-         * subsequent call to {@code previous} would return the new element.
-         * (This call increases by one the value that would be returned by a
-         * call to {@code nextIndex} or {@code previousIndex}.)
-         *
-         * @param o the element to insert
-         * @throws IllegalArgumentException      if some aspect of this element
-         *                                       prevents it from being added to this list
-         */
         @Override
         public void add(Object o) {
             if(o==null) throw new IllegalArgumentException("A null for Object o is not allowed");
@@ -864,7 +761,7 @@ public class ListAdapter implements HList {
 
         @Override
         public HListIterator listIterator() {
-            return this.listIterator();
+            return this.listIterator(fromIndex);
         }
 
         @Override
